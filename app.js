3d22654af7c03aa5d755bb2b2d8d6d1b86b45db2
shamindermundi <punnumundi@gmail.com>
2020-04-04 11:57:59 +0530

    Initial schema and query and mutation of event

--- app.js
+++ app.js
@@ -5,18 +5,36 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   grapgqlHttp({
     schema: buildSchema(`
-        type RootQuery{
-            events: [String!]!
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
         }
 
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date:String!
+        }
+
+        type RootQuery{
+            events: [Event!]!
+        }
+
+
         type RootMutation {
-            createEvent(name: String):String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema{
@@ -26,11 +44,18 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return ["sleeping", "gym", "gaming"];
+        return events;
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
+        };
+        events.push(event);
+        return event;
       },
     },
     graphiql: true,
