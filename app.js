e763aa84c7c9e8af0067b6ea641be3ddc0858209
shamindermundi <punnumundi@gmail.com>
2020-04-06 10:55:41 +0530

    Code to create user and event

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const grapgqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -22,11 +24,22 @@ app.use(
             date: String!
         }
 
+        type User {
+          _id: ID!
+          email: String!
+          password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date:String!
+        }
+
+        input UserInput {
+          email: String!
+          password: String!
         }
 
         type RootQuery{
@@ -36,6 +49,7 @@ app.use(
 
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
 
         schema{
@@ -45,25 +59,59 @@ app.use(
     `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: (args) => {
+        return bcrypt
+          .hash(args, userInput.password, 12)
+          .then((hashedPassword) => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword,
+            });
+            return user.save();
+          })
+          .then((result) => {
+            return { ...result._doc, _id: result.id };
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
 
-const dbUrl = `mongodb+srv://${process.env.MONGO_USER}:mundi123@cluster0-nmbov.mongodb.net/test?retryWrites=true&w=majority`;
+const dbUrl = `mongodb+srv://${process.env.MONGO_USER}:mundi123@cluster0-nmbov.mongodb.net/events-react-dev?retryWrites=true&w=majority`;
+
+console.log("\n\n log", dbUrl);
 
 mongoose
   .connect(dbUrl)
